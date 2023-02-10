8b0e81e3be528cd78d3b024934ca5c987c967dbb
JennSiviseth <jennifer.siviseth@gmail.com>
2023-02-10 13:59:28 -0600

    Added More about me

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -2,6 +2,7 @@ export function AboutMe() {
   return (
     <div className="p-4 w-1/2 m-auto bg-slate-300 text-sky-500 font-extrabold"> 
       <h1>About Me</h1>
+      <h1>More about me</h1>
     </div>
   )
 }
