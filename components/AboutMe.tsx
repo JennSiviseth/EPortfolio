6fa8da632a2b206d55918383a112ef64dab2bbac
JennSiviseth <jennifer.siviseth@gmail.com>
2023-02-14 17:21:33 -0600

    Made bg pink

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,8 +1,8 @@
 export function AboutMe() {
   return (
     <div className="p-4 w-1/2 m-auto bg-slate-300 text-sky-500 font-extrabold"> 
-      <h1>About Me</h1>
-      <h1>More about me</h1>
+      <h1>Jennifer Siviseth</h1>
+      <h1>bitch</h1>
     </div>
   )
 }
